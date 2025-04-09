c8be62ea206879b46fa07ff16f85fc4b9e9498bd
GrassiOS <148152704+GrassiOS@users.noreply.github.com>
2025-04-09 16:48:51 -0700

    Vercel-pls

--- app/edit-text/[id]/page.tsx
+++ app/edit-text/[id]/page.tsx
@@ -1,5 +1,7 @@
 import EditTextClient from './EditTextClient';
 
+// Remove generateStaticParams since we're using server-side rendering
+
 export default function EditTextPage({ params }: { params: { id: string } }) {
   return <EditTextClient id={params.id} />;
 }
